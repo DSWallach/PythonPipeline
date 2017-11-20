f3b1d26d2632e3e4476c5fc28c53b9ff0fe19119
DSWallach <d.s.t.wallach@gmail.com>
2017-11-20 14:19:00 -0500

    Working on framwork

--- wsgiPrebuilt.py
+++ wsgiPrebuilt.py
@@ -5,7 +5,7 @@ def my_app(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    return ['WSGI server is running!']
+    return ['WSGI server is running!', ' Here is a message.', ' \nHere is an additional message.']
 
 
 server = wsgiserver.WSGIServer(my_app)

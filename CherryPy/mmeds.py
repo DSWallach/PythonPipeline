4c2ce3ce801be46629c04562d3dddcbb5def803a
DSWallach <d.s.t.wallach@gmail.com>
2017-11-26 07:49:56 -0500

    Moving to lab repo

--- CherryPy/mmeds.py
+++ CherryPy/mmeds.py
@@ -1,13 +1,14 @@
-from subprocess import system_check_call
+from subprocess import run
 
 
 def check_metadata(file_fp):
     """
     Execute QIIME to check the metadata file provided.
     """
-    call_string = 'validate_mapping_file.py ' + file_fp
+    sac = 'source activate qiime'
+    call_string = sac + 'validate_mapping_file.py ' + file_fp
     try:
-        system_check_call(call_string, shell=True)
+        run(call_string, shell=True, check=True)
         return 0
     except ValueError:
         return 1
